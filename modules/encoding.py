123d85432c3bdffc9ed354d39cb623325bc484f1
nxenon <Khodexenon@gmail.com>
2021-04-21 17:35:08 +0430

    encoding added (Encoder class added --> intagrated with b64)

--- modules/encoding.py
+++ modules/encoding.py
@@ -5,7 +5,9 @@ Encoding functions
 
 import base64
 
-def en_b64(word):
+all_encoding_algs = ['base64']
+
+def en_base64(word):
 
     word = word.strip()
     msg_bytes = word.encode('ascii')
@@ -14,3 +16,39 @@ def en_b64(word):
     b64_msg = b64_bytes.decode('ascii')
 
     return {'encoded':b64_msg ,'word':word}
+
+class Encoder:
+
+    def __init__(self,msg=None):
+        self.msg = msg
+
+    def encode(self):
+        if self.msg:
+            return self.check_alg()
+
+    def check_alg(self):
+        encoding_alg_and_string = self.msg.split(' ')
+        if len(encoding_alg_and_string) == 2:
+            if encoding_alg_and_string[0] in all_encoding_algs :
+                alg = encoding_alg_and_string[0]
+                string = encoding_alg_and_string[1]
+
+                if alg == 'base64':
+                    return en_base64(string)
+
+        else:
+            return None
+
+    def show_help(self):
+        temp_msg = "\n\t\t\t".join(all_encoding_algs)
+        help_msg = '''
+        ```
+z! encode algorithm your_string
+z! encode base64 xenon
+
+    available algorithmes :
+            {}
+        ```
+        '''
+        help_msg = help_msg.format(temp_msg)
+        return help_msg
