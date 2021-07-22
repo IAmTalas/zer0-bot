efa75ab56825e505cd1c91a868285630a02a699b
Amir Talas <amirhoseintalas@gmail.com>
2021-07-22 16:58:21 +0430

    Add check_cve_update func

--- modules/features.py
+++ modules/features.py
@@ -56,3 +56,50 @@ def remove_expired_event():
         valid_list = [item for item in file_json if not (datetime.datetime.strptime(item['time'], r"%Y-%m-%d %H:%M") - datetime.datetime.utcnow()).days <= -1 ]
     with open('modules/events.json', 'w+' ,encoding='utf-8') as file:    
         file.write(json.dumps(valid_list, ensure_ascii=False ,indent=4))
+
+def send_request(url):
+    res =  requests.get(url=url, stream=True, headers={'limit': '30'}).json()['results']
+    for index in range(res):
+        res[index].pop('vulnerable_configuration_cpe_2_2', None)
+        res[index].pop('vulnerable_configuration', None)
+        res[index].pop('vulnerable_product', None)
+        res[index].pop('references', None)
+
+def render_cve_embed(data):
+    embed_items = []
+
+    for cve in data:
+        print(cve)
+        if cve['cvss'] != None:    
+            if float(cve['cvss']) == 0:
+                color = discord.Color.green()
+            elif float(cve['cvss']) in range(1,4):
+                color = discord.Color.from_rgb(255,255,0)
+            elif float(cve['cvss']) in range(4,8):
+                color = discord.Color.orange()
+            elif float(cve['cvss']) in range(7,9):
+                color = discord.Color.red()
+            else:
+                color = discord.Color.from_rgb(165,0,0)
+        else: 
+            color =discord.Color(255,255,255)    
+        embed = discord.Embed(title=f"{cve['id']}", description=f"Published : {cve['Published']}",color=color)
+        embed.add_field(name="summary", value=f"{cve['summary'][:600]}")
+        embed.add_field(name="CVSS", value=f"{cve['cvss']}")
+        embed_items.append(embed)
+    return embed_items
+
+def check_cve_update():
+    global init_request
+    update_list = []
+    req = send_request("https://cve.circl.lu/api/query")
+    print(datetime.datetime.now())
+    if not req[0]['id'] == init_request[0]['id'] :
+        for item in req:
+            if item['id'] == init_request[0]['id'] :
+                break
+            update_list.append(item)
+        init_request = req
+        return render_cve_embed(update_list)
+
+init_request = send_request("https://cve.circl.lu/api/query")
